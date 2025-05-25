cb6457dbadf746dd21fe1bee7fa89e8cd541cee8
mklai10 <mklai10@hotmail.com>
2025-05-24 18:58:25 -0700

    added header for about, refactored code

--- text.js
+++ text.js
@@ -1,10 +1,10 @@
 let titleElement = document.getElementById("title-text");
-let titleText = "Welcome To <br> Matthew's Website";
+let titleText = "Welcome To Matthew's Website";
 
 function typeText(element, text) {
     let place = 1;
     let interval = window.setInterval(function () {
-        element.innerHTML = text.substring(0, place) + " <";
+        element.innerHTML = text.substring(0, place) + " |";
         place++;
         if (place == text.length + 1) {
             clearInterval(interval);
